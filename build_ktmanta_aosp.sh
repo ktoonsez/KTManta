626e5fa6dd8c031db148e0952d0fb0c6542e780e
ktoonsez <ktoonsez@gmail.com>
2012-11-26 20:40:33 -0700

    Upgrade to Linaro 4.7 12.09 toolchain

--- build_ktmanta_aosp.sh
+++ build_ktmanta_aosp.sh
@@ -8,7 +8,7 @@ export PACKAGEDIR=$PARENT_DIR/Packages/AOSP_Manta
 export USE_SEC_FIPS_MODE=true
 export ARCH=arm
 export SUBARCH=arm
-export CROSS_COMPILE=/home/ktoonsez/androidjb/system/prebuilts/gcc/linux-x86/arm/arm-eabi-4.6/bin/arm-eabi-
+export CROSS_COMPILE=$PARENT_DIR/linaro4.7/bin/arm-eabi-
 
 echo "Remove old Package Files"
 rm -rf $PACKAGEDIR/*
