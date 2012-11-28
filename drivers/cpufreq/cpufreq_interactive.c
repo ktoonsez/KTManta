a7d747bde4120127edaae9f3647d8ca51e2a4c99
ktoonsez <ktoonsez@gmail.com>
2012-11-27 21:14:09 -0700

    cpufreq: Change interactive gov so it doesnt destroy sysfs group to keep ROM from getting super pissed

--- drivers/cpufreq/cpufreq_interactive.c
+++ drivers/cpufreq/cpufreq_interactive.c
@@ -702,8 +702,8 @@ static int cpufreq_governor_interactive(struct cpufreq_policy *policy,
 
 		rc = sysfs_create_group(cpufreq_global_kobject,
 				&interactive_attr_group);
-		if (rc)
-			return rc;
+		//if (rc)
+		//	return rc;
 
 		idle_notifier_register(&cpufreq_interactive_idle_nb);
 		break;
@@ -728,8 +728,8 @@ static int cpufreq_governor_interactive(struct cpufreq_policy *policy,
 			return 0;
 
 		idle_notifier_unregister(&cpufreq_interactive_idle_nb);
-		sysfs_remove_group(cpufreq_global_kobject,
-				&interactive_attr_group);
+		//sysfs_remove_group(cpufreq_global_kobject,
+		//		&interactive_attr_group);
 
 		break;
 
