b8850879a62aef428b6cff6e0372c262897ecb78
Shin HATTORI <toplut@gmail.com>
2013-02-25 23:19:44 +0900

    - hard float compile success ( on Ubuntu 12.10 arm-linux-gnueabihf )

--- build_ktmanta_aosp.sh
+++ build_ktmanta_aosp.sh
@@ -8,8 +8,8 @@ export PACKAGEDIR=$PARENT_DIR/Packages/AOSP_Manta
 export USE_SEC_FIPS_MODE=true
 export ARCH=arm
 export SUBARCH=arm
-export CROSS_COMPILE=$PARENT_DIR/linaro4.7/bin/arm-eabi-
-export CFLAGS="-Ofast -mfloat-abi=hard -fstrict-aliasing -ffast-math-fgraphite-identity -floop-block -floop-interchage -floop-strip-mine -ftree-loop-distribution -ftree-loop-linear -ftree-parallelize-loops"
+export CROSS_COMPILE=arm-linux-gnueabihf-
+export CONFIG_SCHED_BFS=y
 
 echo "Remove old Package Files"
 rm -rf $PACKAGEDIR/*
